b8ace34550472248eb2d0881b1522cb0c4b8d6ab
3llena <30310224+3llena@users.noreply.github.com>
2022-12-24 06:51:07 +0100

    moved

---
+++ scripts/setup.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/WongKinYiu/yolov7.git
+cd yolov7
+git clone https://github.com/3llena/yolov7-e6e-custom.git
+pip install -r yolov7-e6e-custom/requirements.txt
+pip install -r yolov7-e6e-custom/requirements_gpu.txt
+wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7.pt
+wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7-e6e.pt
+
